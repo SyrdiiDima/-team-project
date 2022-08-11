6ad8995fdc162040a8904e21ac928a2aff115e6a
SyrdiiDima <dimasyrdij@icloud.com>
2022-08-12 01:03:57 +0300

    complete

--- src/js/dataTeam.js
+++ src/js/dataTeam.js
@@ -14,11 +14,9 @@ import andriiBerezhkoUrl from '/src/images/andriiberezhko.jpeg';
 import andriiBerezhkoDtUrl from '/src/images/andrii-berezhko-1.jpg';
 import andriiBerezhkoPhUrl from '/src/images/andrii-berezhko-2.jpg';
 
-
 import andriiReznichenkoUrl from '../images/ReznichenkoAndrii.jpg';
 import andriiReznichenkoDtUrl from '../images/andrii-reznichenko-1.jpg';
 import andriiReznichenkoPhUrl from '/src/images/andrii-reznichenko-2.jpg';
-
 
 import oxanaDemovaUrl from '/src/images/oxana.jpg';
 import oxanaDemovaDtUrl from '/src/images/oxana-demova-1.jpg';
@@ -32,84 +30,83 @@ import maksymAfanasievUrl from '/src/images/maxsim.jpeg';
 import maksymAfanasievDtUrl from '/src/images/maksim-afanasiev.jpg';
 import maksymAfanasievPhUrl from '/src/images/maksim-afanasiev-2.jpg';
 
-import alexeyCkladanyukUrl from '/src/images/olecsiy.jpeg';
-import alexeyCkladanyukDtUrl from '/src/images/alexey-skladaniuk-1.jpg';
-import alexeyCkladanyukPhUrl from '/src/images/olexiy-skladaniuk-2.jpg';
+import dimaSyrdiiUrl from '/src/images/syrdii.jpg';
+import dimaSyrdiiDtUrl from '/src/images/syrdii-1.jpg';
+import dimaSyrdiiPhUrl from '/src/images/syrdii-1.jpg';
 
 export const modalDataTeam = [
-{
-   url: maximEremeevUrl,
-   laptop: maximEremeevDtUrl,
-   Phone: maximEremeevPhUrl,
-   href: 'https://github.com/Maksmaximusmaxim',
-   nameEmp: 'Maxim Eremeev',
-   position: 'Team Lead', 
-},
-{
-   url: olyaZarichnaUrl,
-   laptop: olyaZarichnaDtUrl,
-   Phone: olyaZarichnaPhUrl,
-   href: 'https://github.com/Olyazarichna',
-   nameEmp: 'Olya Zarichna',
-   position: 'Scrum Master', 
-},
-{
-   url: vitaliiPiddubchakUrl,
-   laptop: vitaliiPiddubchakDtUrl,
-   Phone: vitaliiPiddubchakPhUrl,
-   href: 'https://github.com/Vitaliy28031985',
-   nameEmp: 'Vitalii Piddubchak',
-   position: 'Developer',
-},
-{
-   url: andriiBerezhkoUrl,
-   laptop: andriiBerezhkoDtUrl,
-   Phone: andriiBerezhkoPhUrl,
-   href: 'https://github.com/andriiberezhko',
-   nameEmp: 'Andrii Berezhko',
-   position: 'Developer',
-},
-{
-   url: andriiReznichenkoUrl,
-   laptop: andriiReznichenkoDtUrl,
-   Phone: andriiReznichenkoPhUrl,
-   href: 'https://github.com/4ezeronnik',
-   nameEmp: 'Andrii Reznichenko',
-   position: 'Developer',
-},
-{
-   url: oxanaDemovaUrl,
-   laptop: oxanaDemovaDtUrl,
-   Phone: oxanaDemovaPhUrl,
-   href: 'https://github.com/OxanaDemova',
-   nameEmp: 'Oxana Demova',
-   position: 'Developer',
-},
-{
-   url: volodymyrGanzelevskyiUrl,
-   laptop: volodymyrGanzelevskyiDtUrl,
-   Phone: volodymyrGanzelevskyiPhUrl,
-   href: 'https://github.com/zganz',
-   nameEmp: 'Volodymyr Ganzelevskyi',
-   position: 'Developer',
-},
-{
-   url: maksymAfanasievUrl,
-   laptop: maksymAfanasievDtUrl,
-   Phone: maksymAfanasievPhUrl,
-   href: 'https://github.com/MaksimAfanasiev',
-   nameEmp: 'Maksym Afanasiev',
-   position: 'Developer',
-},
-{
-   url: alexeyCkladanyukUrl,
-   laptop: alexeyCkladanyukDtUrl,
-   Phone: alexeyCkladanyukPhUrl,
-   href: 'https://github.com/Skladaniuk',
-   nameEmp: 'Alexey Skladaniuk',
-   position: 'Developer',
-},
+  {
+    url: maximEremeevUrl,
+    laptop: maximEremeevDtUrl,
+    Phone: maximEremeevPhUrl,
+    href: 'https://github.com/Maksmaximusmaxim',
+    nameEmp: 'Maxim Eremeev',
+    position: 'Team Lead',
+  },
+  {
+    url: olyaZarichnaUrl,
+    laptop: olyaZarichnaDtUrl,
+    Phone: olyaZarichnaPhUrl,
+    href: 'https://github.com/Olyazarichna',
+    nameEmp: 'Olya Zarichna',
+    position: 'Scrum Master',
+  },
+  {
+    url: vitaliiPiddubchakUrl,
+    laptop: vitaliiPiddubchakDtUrl,
+    Phone: vitaliiPiddubchakPhUrl,
+    href: 'https://github.com/Vitaliy28031985',
+    nameEmp: 'Vitalii Piddubchak',
+    position: 'Developer',
+  },
+  {
+    url: andriiBerezhkoUrl,
+    laptop: andriiBerezhkoDtUrl,
+    Phone: andriiBerezhkoPhUrl,
+    href: 'https://github.com/andriiberezhko',
+    nameEmp: 'Andrii Berezhko',
+    position: 'Developer',
+  },
+  {
+    url: andriiReznichenkoUrl,
+    laptop: andriiReznichenkoDtUrl,
+    Phone: andriiReznichenkoPhUrl,
+    href: 'https://github.com/4ezeronnik',
+    nameEmp: 'Andrii Reznichenko',
+    position: 'Developer',
+  },
+  {
+    url: oxanaDemovaUrl,
+    laptop: oxanaDemovaDtUrl,
+    Phone: oxanaDemovaPhUrl,
+    href: 'https://github.com/OxanaDemova',
+    nameEmp: 'Oxana Demova',
+    position: 'Developer',
+  },
+  {
+    url: volodymyrGanzelevskyiUrl,
+    laptop: volodymyrGanzelevskyiDtUrl,
+    Phone: volodymyrGanzelevskyiPhUrl,
+    href: 'https://github.com/zganz',
+    nameEmp: 'Volodymyr Ganzelevskyi',
+    position: 'Developer',
+  },
+  {
+    url: maksymAfanasievUrl,
+    laptop: maksymAfanasievDtUrl,
+    Phone: maksymAfanasievPhUrl,
+    href: 'https://github.com/MaksimAfanasiev',
+    nameEmp: 'Maksym Afanasiev',
+    position: 'Developer',
+  },
+  {
+    url: dimaSyrdiiUrl,
+    laptop: dimaSyrdiiDtUrl,
+    Phone: dimaSyrdiiPhUrl,
+    href: 'https://github.com/SyrdiiDima',
+    nameEmp: 'Dmitrii Syrdii',
+    position: 'Developer',
+  },
 ];
 
 // це коміт
-
